1f2c103619c78213ce3711528bf8a90dc9f5a2c8
omric <omric@netapp.com>
2023-07-12 16:52:36 +0300

    init

---
+++ src/main/java/org/example/internalConfig.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class internalConfig {
+
+    /**
+     * The name of the person
+     */
+    private String name;
+
+    /**
+     * The age of the person
+     */
+    private Integer age;
+
+    /**
+     * The name of work place
+     */
+    private String work;
+}
